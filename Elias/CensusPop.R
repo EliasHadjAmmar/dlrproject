a9ade93a37ca2291520bf1941e90af22a0226524
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-04-29 17:30:13 +0200

    clean up CensusPop.R

--- Elias/CensusPop.R
+++ Elias/CensusPop.R
@@ -7,10 +7,13 @@ grid <- st_read("drive/data/zensus/Zensus_Frankfurt_am_Main_Grid_100m.gpkg")
 nbs <- st_read("drive/data/landprices/Land_Prices_Neighborhood_Frankfurt_am_Main.gpkg")
 census <- read_delim("drive/data/zensus/Zensus_Frankfurt_am_Main_Population.csv", delim=";")
 
+# Spatially join grid cells to neighbourhoods
 keys <- grid |> st_join(nbs, join=st_intersects)
+
+# Join the census data from the csv to the already-joined grid cells
 matched_grid_data <- keys |> left_join(census, by="Grid_Code")
 
-
+# Group by neighbourhood and sum group population counts
 nbs_popsums <- matched_grid_data |> 
   group_by(Neighborhood_FID) |> 
   st_drop_geometry() |> 
@@ -18,10 +21,9 @@ nbs_popsums <- matched_grid_data |>
     across(-c(Grid_Code, City_Code, Land_Value, Area_Types, Area_Count, City_Name, ...1), 
            sum))
 
-nbs_popshares <- nbs_popsums |> 
-  mutate(across(-Neighborhood_FID, \(x) (x/population_total_units)))
+# Compute shares for groups
+census_pop <- nbs_popsums |> 
+  mutate(across(-c(Neighborhood_FID, population_total_units), \(x) (x/population_total_units)))
 
-census_pop <- nbs_popshares |> 
-  mutate(population_total_units = nbs_popsums$population_total_units)
-
+# Write to drive
 write_csv(census_pop, "drive/aggregates/census_pop.csv")
